cfa2c5aa2351243aaa0553b595827a4a34a1f7aa
Mohammed Ghally <mohammedghallydev@gmail.com>
2024-09-04 11:39:58 +0400

    Adding hovers

--- app/components/ClassBooking/ClassBooking.js
+++ app/components/ClassBooking/ClassBooking.js
@@ -2,14 +2,17 @@ import React from "react";
 import styles from "../../styles/ClassBooking/ClassBooking.module.css";
 import ClassBookingText from "./ClassBookingText";
 import ClassBookingForm from "./ClassBookingForm";
+import Image from "next/image";
 
 export default function ClassBooking() {
-    return (
-        <section className={styles.sectionClassBooking}>
-            <div className={`${styles.container} ${styles.grid} ${styles.grid2Cols} ${styles.gridCenterV}`}>
-                <ClassBookingText />
-                <ClassBookingForm />
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.sectionClassBooking}>
+      <div
+        className={`${styles.container} ${styles.grid} ${styles.grid2Cols} ${styles.gridCenterV}`}
+      >
+        <ClassBookingText />
+        <ClassBookingForm />
+      </div>
+    </section>
+  );
 }
